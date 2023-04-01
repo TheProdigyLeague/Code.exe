95d1d6d1f12dde7fffd253bd20d78e0eb52cbad1
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-01 17:08:39 -0500

    upd -re
    a non refundable token

--- Methodology and Resources/侦察.bat
+++ Methodology and Resources/侦察.bat
@@ -23,34 +23,24 @@
   * Nikto
   * Payment functionality
 
-## Passive recon
-
+## Passive
 * Using Shodan (https://www.shodan.io/) to detect similar app
-
   ```bash
-  can be integrated with nmap (https://github.com/glennzw/shodan-hq-nse)
-  nmap --script shodan-hq.nse --script-args 'apikey=<yourShodanAPIKey>,target=<hackme>'
+  $ --nmap (https://github.com/glennzw/shodan-hq-nse)
+  $ -nmap --script shodan-hq.nse --script-args 'apikey=<yourShodanAPIKey>,target=<hackme>'
   ```
-
 * Using The Wayback Machine (https://archive.org/web/) to detect forgotten endpoints
-
   ```bash
-  look for JS files, old links
-  curl -sX GET "http://web.archive.org/cdx/search/cdx?url=<targetDomain.com>&output=text&fl=original&collapse=urlkey&matchType=prefix"
+$ --look {for .JS} #.mum
+$  curl -sX GET "http://web.archive.org/cdx/search/cdx?url=<targetDomain.com>&output=text&fl=original&collapse=urlkey&matchType=prefix"
   ```
-
 * Using The Harvester (https://github.com/laramies/theHarvester)
-
   ```python
-  python theHarvester.py -b all -d domain.com
+>>>theHarvester.py -b all -d domain.com
   ```
-
-## Active recon
-
+## Active
 * [Network discovery](Network%20Discovery.md) with masscan, nmap etc.
-
 * rpcclient
-
   ```bash
   $ rpcclient -U '%' [target host]
   rpcclient $> querydominfo
@@ -58,17 +48,14 @@
   Server: METASPLOITABLE
   Comment: metasploitable server (Samba 3.0.20-Debian)
   Total Users: 35
-
   rpcclient $> enumdomusers
   user:[games] rid:[0x3f2]
   user:[nobody] rid:[0x1f5]
   user:[bind] rid:[0x4ba]
   ```
-
 * enum4linux
-
   ```bash
-  enum4linux v0.8.9 (http://labs.portcullis.co.uk/application/enum4linux/)
+$  enum4linux v0.8.9 (http://labs.portcullis.co.uk/application/enum4linux/)
   Usage: ./enum4linux.pl [options] ip
   -U        get userlist
   -M        get machine list*
@@ -93,110 +80,97 @@
   index: 0x7 Account: www-data Name: www-data Desc: (null)
   index: 0x8 Account: root Name: root Desc: (null)
   ```  
-
 * Zone Transfer
-
   ```powershell
-  host -t ns domain.local
-  domain.local name server master.domain.local.
+>  host -t ns domain.local
+>  domain.local name server master.domain.local.
 
-  host master.domain.local        
-  master.domain.local has address 192.168.1.1
+>  host master.domain.local        
+>  master.domain.local has address 192.168.1.1
  
-  dig axfr domain.local @192.168.1.1
+>  dig axfr domain.local @192.168.1.1
   ```
-
 ## List all the subdirectories and files
-
 * Using BFAC (Backup File Artifacts Checker): An automated tool that checks for backup artifacts that may disclose the web-application's source code.
-
   ```bash
-  git clone https://github.com/mazen160/bfac
-
-  Check a single URL
-  bfac --url http://example.com/test.php --level 4
-
-  Check a list of URLs
-  bfac --list testing_list.txt
+$  git clone https://github.com/mazen160/bfac
+$  # URL
+$  bfac --url http://example.com/test.php --level 4
+$ --list*URLs
+$  bfac --list testing_list.txt
   ```
-
 * Using DirBuster or GoBuster
-
   ```bash
-  ./gobuster -u http://buffered.io/ -w words.txt -t 10
-  -u url
-  -w wordlist
-  -t threads
-
-  More subdomain :
-  ./gobuster -m dns -w subdomains.txt -u google.com -i
-
-  gobuster -w wordlist -u URL -r -e
+$  ./gobuster -u http://buffered.io/ -w words.txt -t 10
+$  -u url
+$  -w wordlist
+$  -t threads
+$ <> [!] subdomain :: ./gobuster -m dns -w subdomains.txt -u google.com -i || gobuster -w wordlist -u URL -r -e</>
   ```
-
 * Using a script to detect all phpinfo.php files in a range of IPs (CIDR can be found with a whois)
-
   ```bash
-  #!/bin/bash
-  for ipa in 98.13{6..9}.{0..255}.{0..255}; do
-  wget -t 1 -T 3 http://${ipa}/phpinfo.php; done &
+$  #!/bin/bash
+$  for ipa in 98.13{6..9}.{0..255}.{0..255}; do
+$  wget -t 1 -T 3 http://${ipa}/phpinfo.php; done &
   ```
-
 * Using a script to detect all .htpasswd files in a range of IPs
-
   ```bash
-  #!/bin/bash
-  for ipa in 98.13{6..9}.{0..255}.{0..255}; do
-  wget -t 1 -T 3 http://${ipa}/.htpasswd; done &
+$  #!/bin/bash
+$  for ipa in 98.13{6..9}.{0..255}.{0..255}; do
+$  wget -t 1 -T 3 http://${ipa}/.htpasswd; done &
   ```
-
 ## Looking for Web vulnerabilities
-
 * Look for private information in GitHub repos with GitRob
-
   ```bash
-  git clone https://github.com/michenriksen/gitrob.git
-  gitrob analyze johndoe --site=https://github.acme.com --endpoint=https://github.acme.com/api/v3 --access-tokens=token1,token2
+$  -git clone https://github.com/michenriksen/gitrob.git
+$ --gitrob analyze johndoe --site=https://github.acme.com --endpoint=https://github.acme.com/api/v3 --access-tokens=token1,token2
   ```
-
 * Explore the website with a proxy (ZAP/Burp Suite)
-  1. Start proxy, visit the main target site and perform a Forced Browse to discover files and directories
-  2. Map technologies used with Wappalyzer and Burp Suite (or ZAP) proxy
-  3. Explore and understand available functionality, noting areas that correspond to vulnerability types
-
+#  1. Start proxy, visit the main target site and perform a Forced Browse to discover files and directories
+##  2. Map technologies used with Wappalyzer and Burp Suite (or ZAP) proxy
+###  3. Explore and understand available functionality, noting areas that correspond to vulnerability types
     ```bash
-    Burp Proxy configuration on port 8080 (in .bashrc):
-    alias set_proxy_burp='gsettings set org.gnome.system.proxy.http host "http://localhost";gsettings set org.gnome.system.proxy.http port 8080;gsettings set org.gnome.system.proxy mode "manual"'
-    alias set_proxy_normal='gsettings set org.gnome.system.proxy mode "none"'
-
+$ --.c -.h --Burp_Proxy.config on port:: [8080] (in .bashrc):
+>    alias set_proxy_burp='gsettings set org.gnome.system.proxy.http host "http://localhost";gsettings set org.gnome.system.proxy.http port 8080;gsettings set org.gnome.system.proxy mode "manual"'
+>    alias set_proxy_normal='gsettings set org.gnome.system.proxy mode "none"'
+{
     then launch Burp with : java -jar burpsuite_free_v*.jar &
-    ```
-
+};
+ ```
+{void}
+[NUL]
 * [WAHH Task Checklist](https://gist.github.com/gbedoya/10935137) copied from http://mdsec.net/wahh/tasks.html
-
 * Subscribe to the site and pay for the additional functionality to test
-
 * Launch a Nikto scan in case you missed something
-
   ```powershell
-  nikto -h http://domain.example.com
+PS>  nikto -h http://domain.example.com
   ```
-
-* Payment functionality - [@gwendallecoguic](https://twitter.com/gwendallecoguic/status/988138794686779392)
-  > if the webapp you're testing uses an external payment gateway, check the doc to find the test credit numbers, purchase something and if the webapp didn't disable the test mode, it will be free
-
-  From https://stripe.com/docs/testing#cards : "Use any of the following test card numbers, a valid expiration date in the future, and any random CVC number, to create a successful payment. Each test card's billing country is set to U.S. "
-  e.g :
-
-Test card numbers and tokens  
-
+* Payment 
+1001 1111
+- [@gwendallecoguic](https://twitter.com/gwendallecoguic/status/988138794686779392)
+> { if /web/app (You) .test [USE] .ext\  
+$ gateway -check /.doc *find .test -cred [INtEGEr];
+$ ####purchase something
+'&&' {if /web/app [NO] -disable .test.mode then, free };
+>>>From https://stripe.com/docs/testing#cards 
+[!]
+PROPIETARY :: $true 
+END-USER :: TEST
+CARDHOLDER :: $true
+PAYMENT :: BILL
+LOCATION :: UNITED STATES OF AMERICA
+### e.g. 
+             <! -- Test-Card, Numbers, Tokens -->
+$cd
+$ls
 | NUMBER           | BRAND          | TOKEN          |
 | :-------------   | :------------- | :------------- |
 | 4242424242424242 | Visa           | tok_visa       |
 | 4000056655665556 | Visa (debit)   | tok_visa_debit |
 | 5555555555554444 | Mastercard     | tok_mastercard |
-
-International test card numbers and tokens     
+.h
+.c
+<! -- International test card numbers and tokens -->     
 
 | NUMBER           | TOKEN          | COUNTRY        | BRAND          |
 | :-------------   | :------------- | :------------- | :------------- |
@@ -205,8 +179,6 @@ International test card numbers and tokens
 | 4000002080000001 | tok_dk         | Denmark (DK)   | Visa           |
 | 4000002460000001 | tok_fi         | Finland (FI)   | Visa           |
 | 4000002500000003 | tok_fr         | France (FR)    | Visa           |
-
 ## References
-
 * [[BugBounty] Yahoo phpinfo.php disclosure - Patrik Fehrenbach](http://blog.it-securityguard.com/bugbounty-yahoo-phpinfo-php-disclosure-2/)
 * [Nmap CheatSheet - HackerTarget](https://hackertarget.com/nmap-cheatsheet-a-quick-reference-guide/)
