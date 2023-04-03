deae89fdf902d37ed6acb367004708c3af70221a
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-02 22:04:13 -0500

    upd -re --mod
    infection injection projection

--- gang_bangers.c
+++ gang_bangers.c
@@ -1,1 +1,33 @@
 >>>import -all --injections
+$ --mod -force --1njectionS
+>>>import 1njectionS
+$ run index.js@--github/include-fragment-element/d...
+$clipboarditem.js@--github/browser-support/lib/token
+$ --git clone https://github_remote-modules/assets/manifest.json
+$--git clone https://github.com/TheProdigyLeague/Jwt_aws/blob/hash-cracking/CSV%20Injection/dat.bat
+>>>on \swissky@xCSV Injection/dat.bat
+>>>on \swissky@xSQL Injection/evil.bat
+>>>on \swissky@xPATH Injection/dir.bash
+>>>on \swissky@xAWS/BucketS3_injection.js
+>>>on \swissky@xXSS Injection.py
+>>>on \swissky@xXXE Injection.py
+>>>on \swissky@xCRLF Injection.py
+>>>on \swissky@xSAML Injection.py
+>>>on \swissky@xXSLT Injection.py
+>>>on \swissky@xNoSQL Injection.py
+>>>on \swissky@xCMD Injection.py
+>>>on \swissky@xGraphQL Injection.py
+>>>on \swissky@XlatEX Injection.py
+>>>on \swissky@Cross-Origin Injection/misconfig.c
+>>>on \swissky@/README.md -list --upd
+$ ls
+去猎犬！机器使用！
+<pre class="">          ,
+         _o_
+    ._ ,'   `o'
+----(_)      :       ^aNT
+    '  `.   .o
+         ~o~  `
+          '
+</pre>
+之内。投影。
